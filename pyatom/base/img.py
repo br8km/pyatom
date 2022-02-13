0d977ea3ae04bb89a70a8b18f6af1dbc8fcba6ef
br8km <59230598+br8km@users.noreply.github.com>
2022-02-13 19:10:24 +0800

    img.py add test case

--- pyatom/base/img.py
+++ pyatom/base/img.py
@@ -4,17 +4,20 @@ import random
 import hashlib
 from pathlib import Path
 from typing import Union
+from abc import ABC
 
 import filetype
 from PIL import Image, ImageSequence
+from PIL.Image import Image as ImageObj
 import imageio
+import numpy as np
 
 
-__all__ = ("Rehash",)
+__all__ = ("Rehasher",)
 
 
-class ImageBase:
-    """Base cls for Image Operation."""
+class AbsImage(ABC):
+    """Abstract cls for Image Operation."""
 
     @staticmethod
     def read_bytes(file_image: Path) -> bytes:
@@ -47,7 +50,7 @@ class ImageBase:
         return file_type.lower().split("/")[1] if file_type else ""
 
 
-class Rehash(ImageBase):
+class Rehasher(AbsImage):
     """Simple Example Code for Rehash Image by random pixels.
 
     To fight against new algorithm like this:
@@ -116,10 +119,12 @@ class Rehash(ImageBase):
                 width, height = frame.width, frame.height
                 for _ in range(number):
                     pos = self.rnd_pos(width, height)
-                    pixel_old = image.getpixel(xy=pos)
+                    pixel_old = frame.getpixel(xy=pos)
                     pixel_new = self.new_pixel(pixel_old)
                     frame.putpixel(xy=pos, value=pixel_new)
-                list_frame.append(frame)
+
+                # for better compitable with linux
+                list_frame.append(np.array(frame))
 
             # for better quality than using PIL image.save method.
             imageio.mimsave(
@@ -132,3 +137,112 @@ class Rehash(ImageBase):
             return file_new, self.read_bytes(file_new)
 
         raise TypeError(f"image format {image.format} not support!")
+
+
+class TestRehash:
+    """TeseCase for Rehash Image."""
+
+    width = 100
+    height = 100
+    frames = 10
+
+    rehasher = Rehasher()
+
+    def gen_image(self) -> ImageObj:
+        """Generate new image with random color."""
+        return Image.new(
+            mode="RGB", size=(self.width, self.height), color=self.rehasher.rnd_pixel()
+        )
+
+    def test_png_rehash(self) -> None:
+        """Test rehash png image."""
+        file_old = Path(Path(__file__).parent, "old.png")
+        file_new = Path(Path(__file__).parent, "new.png")
+
+        assert file_old.is_file() is False
+        assert file_new.is_file() is False
+
+        img = self.gen_image()
+        img.save(fp=file_old, format="PNG")
+        assert file_old.is_file()
+        hash_old = self.rehasher.get_hash(file_old)
+        assert hash_old
+
+        file_new, obj_new = self.rehasher.new_image(file_old, file_new)
+        assert file_new.is_file()
+
+        hash_new = self.rehasher.get_hash(obj_new)
+        assert hash_new
+        assert hash_old != hash_new
+
+        file_old.unlink(missing_ok=True)
+        file_new.unlink(missing_ok=True)
+
+        assert file_old.is_file() is False
+        assert file_new.is_file() is False
+
+    def test_jpg_rehash(self) -> None:
+        """Test rehash jpg image."""
+        file_old = Path(Path(__file__).parent, "old.jpg")
+        file_new = Path(Path(__file__).parent, "new.jpg")
+
+        assert file_old.is_file() is False
+        assert file_new.is_file() is False
+
+        img = self.gen_image()
+        img.save(fp=file_old, format="JPEG")
+        assert file_old.is_file()
+        hash_old = self.rehasher.get_hash(file_old)
+        assert hash_old
+
+        file_new, obj_new = self.rehasher.new_image(file_old, file_new)
+        assert file_new.is_file()
+
+        hash_new = self.rehasher.get_hash(obj_new)
+        assert hash_new
+        assert hash_old != hash_new
+
+        file_old.unlink(missing_ok=True)
+        file_new.unlink(missing_ok=True)
+
+        assert file_old.is_file() is False
+        assert file_new.is_file() is False
+
+    def test_gif_rehash(self) -> None:
+        """Test rehash gif image."""
+        file_old = Path(Path(__file__).parent, "old.gif")
+        file_new = Path(Path(__file__).parent, "new.gif")
+
+        assert file_old.is_file() is False
+        assert file_new.is_file() is False
+
+        img, *imgs = [self.gen_image() for _ in range(self.frames)]
+        img.save(
+            fp=file_old,
+            format="GIF",
+            append_images=imgs,
+            save_all=True,
+            druation=100,
+            loop=1,
+        )
+
+        assert file_old.is_file()
+        hash_old = self.rehasher.get_hash(file_old)
+        assert hash_old
+
+        file_new, obj_new = self.rehasher.new_image(file_old, file_new)
+        assert file_new.is_file()
+
+        hash_new = self.rehasher.get_hash(obj_new)
+        assert hash_new
+        assert hash_old != hash_new
+
+        file_old.unlink(missing_ok=True)
+        file_new.unlink(missing_ok=True)
+
+        assert file_old.is_file() is False
+        assert file_new.is_file() is False
+
+
+if __name__ == "__main__":
+    app = TestRehash()
