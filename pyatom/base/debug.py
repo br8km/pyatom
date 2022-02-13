f9450eaaf7487d7bb70d131530cde08fda028dd6
br8km <59230598+br8km@users.noreply.github.com>
2022-02-13 20:48:48 +0800

    test case add

--- pyatom/base/debug.py
+++ pyatom/base/debug.py
@@ -16,55 +16,56 @@ __all__ = ("Debugger",)
 class Debugger:
     """Debugger to generate string identities"""
 
-    __slots__ = ("__dict__", "path", "name", "length", "sid")
+    __slots__ = (
+        "path",
+        "name",
+        "length",
+        "id_int",
+        "id_str",
+    )
 
     def __init__(self, path: Path, name: str, length: int = 4) -> None:
         self.path = path
-        self.name = name
         self.length = length
-        self.sid = ""
+        self.name = name if name else self.rnd_name()
+
+        self.id_int = 0
+        self.id_str = self.id2str()
 
     @staticmethod
     def log(message: Any) -> None:
         """logging message for now"""
         now = arrow.now().format("YYYY-MM-DD HH:mm:ss")
-        print(f"{now} - {message}")
+        print(f"{now} - {str(message)}")
 
-    def sid_init(self) -> str:
-        """initialize sid"""
+    def rnd_name(self) -> str:
+        """Generate self.name if empty."""
         seed = string.ascii_letters + string.digits
-        if not self.name:
-            rnd = [random.choice(seed) for _ in range(self.length)]
-            self.name = "".join(rnd)
-        suffix = "0".rjust(self.length, "0")
+        rnd = [random.choice(seed) for _ in range(self.length)]
+        return "".join(rnd)
+
+    def id2str(self) -> str:
+        """Transfer id int number to string."""
+        suffix = "{}".format(self.id_int).rjust(self.length, "0")
         return f"{self.name}-{suffix}"
 
-    def sid_add(self, sid: str) -> str:
-        """increase right side of sid by 1"""
-        suffix_str = sid.split("-")[1]
-        suffix_int = int(suffix_str) + 1
-        suffix = "{}".format(suffix_int).rjust(self.length, "0")
-        return "{}-{}".format(self.name, suffix)
-
-    def sid_new(self) -> str:
+    def id_add(self) -> None:
         """
-        Generate sid fomrat: `[char]-[number]`
+        Add 1 to id_int and generate new id_str fomrat: `[string]-[number]`
         example: abcd-0000, abcd-0001, abcd-0002
         for a long session, set number >= 6
         Usually call before save when need a new file name for debugging
         """
-        if self.sid and "-" in self.sid:
-            self.sid = self.sid_add(self.sid)
-        else:
-            self.sid = self.sid_init()
-        return self.sid
+        self.id_int += 1
+        self.id_str = self.id2str()
+
+    def to_file(self) -> Path:
+        """Generate file path from self.id_str."""
+        return Path(self.path, self.id_str + ".debug")
 
     def save(self, data: Union[str, list, dict], encoding: str = "utf8") -> bool:
         """save data to file inside debug directory"""
-        if not self.sid:
-            raise ValueError("debugger sid not set!")
-
-        file_name = Path(self.path, self.sid)
+        file_name = self.to_file()
         with open(file_name, "w", encoding=encoding) as file:
             try:
                 file.write(json.dumps(data, indent=2))
@@ -73,3 +74,38 @@ class Debugger:
         result = file_name.is_file()
         self.log(f"[{result}]save debug: {file_name}")
         return result
+
+
+class TestDebugger:
+    """Test Debugger."""
+
+    name = "test"
+    debugger = Debugger(path=Path(__file__).parent, name=name)
+
+    def test_debugger(self) -> None:
+        """Test debugger methods."""
+        assert self.debugger.name
+        assert self.debugger.id_int == 0
+        assert self.debugger.id_str.endswith("0" * self.debugger.length)
+
+        for index in range(10):
+            self.debugger.id_add()
+            assert self.debugger.id_str.endswith(str(index + 1))
+
+        file_debug = self.debugger.to_file()
+        assert file_debug.suffix == ".debug"
+
+        assert self.debugger.save(data={"key": "value"})
+
+        assert self.debugger.save(data={"key": ["value"]})
+
+        assert self.debugger.save(data=["value"])
+
+        assert self.debugger.save(data="value")
+
+        file_debug.unlink(missing_ok=True)
+        assert file_debug.is_file() is False
+
+
+if __name__ == "__main__":
+    app = TestDebugger()
