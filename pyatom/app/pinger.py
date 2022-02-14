cc3790d696017f5d4863258a69d93208bd091abe
br8km <59230598+br8km@users.noreply.github.com>
2022-02-14 20:30:52 +0800

    optimize code and add test case

--- pyatom/app/pinger.py
+++ pyatom/app/pinger.py
@@ -6,9 +6,9 @@
 import random
 import time
 from pathlib import Path
-from base64 import encodebytes
+from urllib.parse import urlparse
 from dataclasses import dataclass, asdict
-from urllib.parse import unquote_to_bytes, urlparse
+from typing import Union, Tuple, Dict, Any
 from http.client import HTTPConnection, HTTPSConnection
 from xmlrpc.client import (
     Transport,
@@ -19,41 +19,18 @@ from xmlrpc.client import (
     Fault,
 )
 from xml.parsers.expat import ExpatError
-from typing import Union, Tuple, Dict, Any
 
 from tldextract import extract
 
-from pyatom.base.log import Logger
-from pyatom.base.io import (
-    load_dict,
-    save_dict,
+from pyatom.base.io import save_list_dict, load_list_dict
+from pyatom.base.log import Logger, init_logger
+from pyatom.base.proxy import HttpProxy, to_auth_header
+
+
+__all__ = (
+    "Service",
+    "XMLPinger",
 )
-from pyatom.base.proxy import HttpProxy
-
-
-__all__ = ("Pinger",)
-
-
-def normalize(url: str) -> str:
-    """Normalize url string."""
-    out = urlparse(url)
-    if out.scheme not in ("http", "https"):
-        return ""
-    return url[:-1] if url.endswith("/") else url
-
-
-def to_proxy_headers(usr: str, pwd: str) -> dict:
-    """Generate proxy headers."""
-    if not usr and not pwd:
-        return {}
-    if usr and pwd:
-        auth_str = f"{usr}:{pwd}"
-        auth_bytes = unquote_to_bytes(auth_str)
-        auth_str = encodebytes(auth_bytes).decode("utf-8")
-        auth_str = "".join(auth_str.split())  # get rid of whitespace
-        return {"Proxy-Authorization": "Basic " + auth_str}
-
-    raise ValueError(f"proxy auth error: `{usr}`:`{pwd}`")
 
 
 @dataclass
@@ -64,19 +41,20 @@ class Service:
     geo: str
     alive: bool
     timestamp: int
-    err: str
+    error: str
 
 
 class HTTPProxyTransport(Transport):
     """HTTP Proxy Transport."""
 
-    def __init__(self, proxy_str: str, user_agent: str, time_out: int):
+    def __init__(self, user_agent: str, proxy_str: str, time_out: int):
         """Init."""
         Transport.__init__(self)
 
-        self.proxy = HttpProxy(proxy_str=proxy_str)
         self.user_agent = user_agent
-        self.proxy_headers = to_proxy_headers(self.proxy.usr, self.proxy.pwd)
+        self.proxy = HttpProxy(proxy_str=proxy_str)
+        key, value = to_auth_header(usr=self.proxy.usr, pwd=self.proxy.pwd)
+        self.proxy_headers = {key: value}
         self.timeout = time_out
 
     def make_connection(
@@ -93,13 +71,14 @@ class HTTPProxyTransport(Transport):
 class HTTPSProxyTransport(SafeTransport):
     """HTTPS Proxy Transport."""
 
-    def __init__(self, proxy_str: str, user_agent: str, time_out: int):
+    def __init__(self, user_agent: str, proxy_str: str, time_out: int):
         """Init."""
         SafeTransport.__init__(self)
 
-        self.proxy = HttpProxy(proxy_str=proxy_str)
         self.user_agent = user_agent
-        self.proxy_headers = to_proxy_headers(self.proxy.usr, self.proxy.pwd)
+        self.proxy = HttpProxy(proxy_str=proxy_str)
+        key, value = to_auth_header(usr=self.proxy.usr, pwd=self.proxy.pwd)
+        self.proxy_headers = {key: value}
         self.timeout = time_out
 
     def make_connection(
@@ -113,70 +92,108 @@ class HTTPSProxyTransport(SafeTransport):
         return connection
 
 
-class Pinger:
-    """XML-RPC Pinger for ping services."""
+class BasePinger:
+    """Base Pinger."""
 
-    time_out = 30
-
-    list_service_str = (
-        "http://rpc.pingomatic.com",
-        "https://rpc.twingly.com/",
-        "http://ping.blo.gs/",
-        "http://www.blogdigger.com/RPC2",
-        #  "https://feedburner.google.com/fb/a/ping"
-        #  "https://www.pingmyblog.com/",
-        #  "http://ping.feedburner.com",
-        #  "http://www.weblogues.com/RPC/",
-        #  "http://pingoat.com/goat/RPC2",
-    )
-
-    def __init__(
-        self, list_ua: list[str], list_px: list[str], file_service: Path, logger: Logger
-    ) -> None:
-        """Init Pinger."""
-
+    def __init__(self, list_ua: list[str], list_px: list[str]) -> None:
+        """Init Base Pinger."""
         self.list_ua = list_ua
         self.list_px = list_px
-        self.file_service = file_service
-        self.logger = logger
 
-        self.service_data = self.load_services()
-
-    def to_ua(self) -> str:
-        """Generate random User-Agent string."""
+    @property
+    def rnd_ua(self) -> str:
+        """Get random user_agent string."""
         return random.choice(self.list_ua)
 
-    def to_px(self) -> str:
-        """Generate random Proxy string."""
+    @property
+    def rnd_px(self) -> str:
+        """Get random proxy_str string."""
         return random.choice(self.list_px)
 
-    def load_services(self) -> dict[str, Service]:
-        """Load dict of service as dict.
-        # keys:  `service` string, `geo` string, `alive` bool, `timestamp` int
-        """
-        if not self.file_service.is_file():
-            return {}
+    @staticmethod
+    def normalize(url: str) -> str:
+        """Normalize url string."""
+        out = urlparse(url)
+        if out.scheme not in ("http", "https"):
+            return ""
+        return url[:-1] if url.endswith("/") else url
 
-        data = load_dict(self.file_service)
-        return {
-            service_url: Service(
+    @staticmethod
+    def load_services(file_service: Path) -> list[Service]:
+        """Load list of Service from local file."""
+        service_data = load_list_dict(file_service)
+        return [
+            Service(
                 url=item.get("url", ""),
                 geo=item.get("geo", ""),
                 alive=item.get("alive", False),
                 timestamp=item.get("timestamp", 0),
-                err=item.get("err", ""),
+                error=item.get("error", ""),
             )
-            for service_url, item in data.items()
-        }
+            for item in service_data
+        ]
 
-    def save_services(self, service_data: dict[str, Service]) -> None:
-        """Save list of service as dict."""
-        save_dict(
-            file_name=self.file_service,
-            file_data={
-                service_url: asdict(service)
-                for service_url, service in service_data.items()
-            },
+    @staticmethod
+    def save_services(file_service: Path, list_service: list[Service]) -> bool:
+        """Save list of Service into local file."""
+        service_data = [asdict(service) for service in list_service]
+        save_list_dict(file_name=file_service, file_data=service_data)
+        return file_service.is_file()
+
+    @staticmethod
+    def to_geo(url: str) -> str:
+        """Get url domain geo string."""
+        out = extract(url)
+        return str(out.suffix)
+
+    def to_service(self, url: str) -> Service:
+        """Get Service from url."""
+        return Service(
+            url=url,
+            geo=self.to_geo(url),
+            alive=False,
+            timestamp=int(time.time()),
+            error="",
+        )
+
+
+class XMLPinger(BasePinger):
+    """XML-RPC Pinger for ping services."""
+
+    __slots__ = (
+        "list_ua",
+        "list_px",
+        "logger",
+        "time_out",
+    )
+
+    def __init__(
+        self, list_ua: list[str], list_px: list[str], logger: Logger, time_out: int = 30
+    ) -> None:
+        """Init XML RPC Pinger."""
+        super().__init__(list_ua=list_ua, list_px=list_px)
+
+        self.logger = logger
+        self.time_out = time_out
+
+    def to_client(self, service_url: str) -> ServerProxy:
+        """Generate ServerProxy client."""
+        if service_url.startswith("https"):
+            return ServerProxy(
+                service_url,
+                transport=HTTPSProxyTransport(
+                    user_agent=self.rnd_ua,
+                    proxy_str=self.rnd_px,
+                    time_out=self.time_out,
+                ),
+            )
+        return ServerProxy(
+            service_url,
+            transport=HTTPProxyTransport(
+                user_agent=self.rnd_ua,
+                proxy_str=self.rnd_px,
+                time_out=self.time_out,
+            ),
         )
 
     def basic_ping(self, client: ServerProxy, site_name: str, home_url: str) -> Any:
@@ -219,9 +236,11 @@ class Pinger:
         return True, response_str
 
     def ping(
-        self, client: ServerProxy, site_name: str, home_url: str, post_url: str
+        self, service: Service, site_name: str, home_url: str, post_url: str
     ) -> tuple[bool, str]:
         """weblogUpdates.extendedPing and weblogUpdates.ping method."""
+        client = self.to_client(service.url)
+
         response = self.extended_ping(
             client=client, site_name=site_name, home_url=home_url, post_url=post_url
         )
@@ -234,119 +253,132 @@ class Pinger:
         )
         return self.parse_respnose(response)
 
-    def to_client(
-        self, service: Service, proxy_str: str, user_agent: str
-    ) -> ServerProxy:
-        """Generate ServerProxy client."""
-        if service.url.startswith("https"):
-            return ServerProxy(
-                service.url,
-                transport=HTTPSProxyTransport(
-                    proxy_str=proxy_str, user_agent=user_agent, time_out=self.time_out
-                ),
-            )
-        return ServerProxy(
-            service.url,
-            transport=HTTPProxyTransport(
-                proxy_str=proxy_str, user_agent=user_agent, time_out=self.time_out
-            ),
-        )
-
     def pinging(
-        self, site_name: str, home_url: str, post_url: str, strict: bool = False
+        self,
+        list_service: list[Service],
+        site_name: str,
+        home_url: str,
+        post_url: str,
+        strict: bool = False,
     ) -> tuple[bool, float]:
-        """Send ping request, Return tuple of success and success ratio."""
-        alive, good = 0, 0
-        for _, service in self.service_data.items():
-            if service.alive:
-                alive += 1
-                client = self.to_client(
-                    service=service, proxy_str=self.to_px(), user_agent=self.to_ua()
-                )
-                okay, _ = self.ping(
-                    client=client,
-                    site_name=site_name,
-                    home_url=home_url,
-                    post_url=post_url,
-                )
-                if okay:
-                    good += 1
-        #  print(f"alive = {alive}, success = {success}")
-        if not alive:
-            warn_message = "Pinger.service alive = 0"
-            self.logger.warning(warn_message)
-            return False, 0.0
+        """Send ping request to alive services, Return tuple of success and success ratio."""
+        total, good = len(list_service), 0
+        for service in list_service:
+            okay, _ = self.ping(
+                service=service,
+                site_name=site_name,
+                home_url=home_url,
+                post_url=post_url,
+            )
+            if okay:
+                good += 1
 
-        success = bool(good == alive if strict else good > 0)
-        ratio = float(good / alive)
+        success = bool(good == total if strict else good > 0)
+        ratio = float(good / total)
         return success, ratio
-
-    @staticmethod
-    def to_geo(url: str) -> str:
-        """Get url domain geo string."""
-        out = extract(url)
-        return str(out.suffix)
 
     def check_service(self, service: Service) -> tuple[bool, str]:
         """Check service alive."""
         site_name = "Github"
         home_url = "https://github.com/"
         post_url = "https://github.com/about"
-        client = self.to_client(
-            service=service,
-            proxy_str=self.to_px(),
-            user_agent=self.to_ua(),
-        )
         return self.ping(
-            client=client,
+            service=service,
             site_name=site_name,
             home_url=home_url,
             post_url=post_url,
         )
 
-    def checking_service(
-        self,
-        list_service_url: list[str],
-        including_exist: bool = False,
-        save_only_success: bool = False,
-    ) -> None:
-        """Checking list of new service url and save into local json file."""
-        checked, good = 0, 0
-        if including_exist:
-            list_service_url.extend(self.service_data.keys())
-
-        list_service_url = [normalize(url) for url in list_service_url]
+    def checking_service(self, list_service_url: list[str]) -> list[Service]:
+        """Checking list of new service url, Return list of Service."""
+        list_service_url = [self.normalize(url) for url in list_service_url]
         list_service_url = [url for url in list_service_url if url]
         list_service_url = list(set(list_service_url))
+        if not list_service_url:
+            self.logger.warning("no valid service urls.")
+            return []
+
+        checked, good = 0, 0
+        list_service: list[Service] = []
         list_service_url = sorted(list_service_url)
         checked = len(list_service_url)
         self.logger.info(f"<{checked}>list_service_url to check...")
 
-        service_data: dict[str, Service] = self.service_data
-
         for index, service_url in enumerate(list_service_url):
-            service = self.service_data.get(
-                service_url,
-                Service(
-                    url=service_url,
-                    geo=self.to_geo(service_url),
-                    alive=False,
-                    timestamp=int(time.time()),
-                    err="",
-                ),
-            )
+            service = self.to_service(service_url)
             success, response_str = self.check_service(service)
-            if success:
-                good += 1
             self.logger.info(f"<{index}>[{success}] `{service.url}` -- {response_str}")
             service.alive = success
-            service.err = response_str
-            service_data[service_url] = service
+            service.error = response_str
+            list_service.append(service)
 
-        if save_only_success:
-            service_data = {
-                url: service for url, service in service_data.items() if service.alive
-            }
+            if success:
+                good += 1
 
-        self.save_services(service_data)
         self.logger.info(f"<{checked}>service checked. <{good}>good found.")
+        return list_service
+
+
+class TestPinger:
+    """TestCase for Pinger."""
+
+    dir_app = Path(__file__).parent
+    logger = init_logger(name="test")
+
+    user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.97 Safari/537.36"
+    proxy_str = "bxusr017:bxpwd017@172.245.255.158:80"
+
+    list_service_url = [
+        "http://rpc.pingomatic.com",
+        "https://rpc.twingly.com/",
+        #  "http://ping.blo.gs/",  # Failed at 2022-02-14
+        "http://www.blogdigger.com/RPC2",
+    ]
+
+    def test_base_pinger(self) -> None:
+        """Test BasePinger."""
+        pinger = BasePinger(list_ua=[self.user_agent], list_px=[self.proxy_str])
+        assert self.user_agent == pinger.rnd_ua
+        assert self.proxy_str == pinger.rnd_px
+
+        assert pinger.normalize("ftp://hello.com") == ""
+        assert pinger.normalize("http://bing.com/") == "http://bing.com"
+
+        list_service = [pinger.to_service(url) for url in self.list_service_url]
+        file_service = Path(self.dir_app, "service.json")
+        pinger.save_services(file_service, list_service)
+        assert file_service.is_file()
+        assert list_service == pinger.load_services(file_service)
+        file_service.unlink(missing_ok=True)
+        assert file_service.is_file() is False
+
+    def test_xml_pinger(self) -> None:
+        """Test XMLPinger."""
+        pinger = XMLPinger(
+            list_ua=[self.user_agent], list_px=[self.proxy_str], logger=self.logger
+        )
+        list_service = [pinger.to_service(url) for url in self.list_service_url]
+        assert len(list_service) == len(self.list_service_url)
+        list_client = [pinger.to_client(url) for url in self.list_service_url]
+        assert len(list_client) == len(self.list_service_url)
+
+        # Check service alive.
+        for service in list_service:
+            success, _ = pinger.check_service(service)
+            assert success is True
+
+        # Checking list service.
+        list_service = pinger.checking_service(self.list_service_url)
+        assert len(list_service) == len(self.list_service_url)
+        assert any(service.alive is True for service in list_service)
+
+        site_name = "Github"
+        home_url = "https://github.com/"
+        post_url = "https://github.com/about"
+        success, ratio = pinger.pinging(list_service, site_name, home_url, post_url)
+        assert success is True
+        assert ratio > 0
+
+
+if __name__ == "__main__":
+    TestPinger()
