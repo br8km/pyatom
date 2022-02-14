52ba4625c8de62fed08fcd7f9c1b82e7b94f0dcb
br8km <59230598+br8km@users.noreply.github.com>
2022-02-14 10:44:55 +0800

    test case add

--- pyatom/base/utils.py
+++ pyatom/base/utils.py
@@ -2,13 +2,13 @@
     Common Operation
 """
 
-from typing import Any, Dict, List, Union
+import random
+from dataclasses import dataclass, asdict
 
 import orjson
 
 __all__ = (
     "print2",
-    "split_list",
     "split_list_int",
     "split_list_str",
     "to_dict",
@@ -25,24 +25,17 @@ def print2(data: dict, extra_line: bool = True) -> None:
         print(orjson.dumps(data, option=opt).decode())
 
 
-def split_list(
-    list_obj: list[Union[str, int]], number: int
-) -> list[list[Union[str, int]]]:
-    """Split list of string or integer into list of list of item."""
-    return [list_obj[i : i + number] for i in range(0, len(list_obj), number)]
-
-
 def split_list_int(list_obj: list[int], number: int) -> list[list[int]]:
-    """split list of int into list of list of int"""
+    """split list of int into list of list of int, each group include number of item."""
     return [list_obj[i : i + number] for i in range(0, len(list_obj), number)]
 
 
-def split_list_str(list_obj: List[str], number: int) -> List[List[str]]:
-    """split list of str into list of list of str"""
+def split_list_str(list_obj: list[str], number: int) -> list[list[str]]:
+    """split list of str into list of list of str, each group include number of item."""
     return [list_obj[i : i + number] for i in range(0, len(list_obj), number)]
 
 
-def to_dict(obj: object) -> Dict[str, Any]:
+def to_dict(obj: object) -> dict:
     """parse object attributes into dictionary"""
     data = {}
     for key in dir(obj):
@@ -58,6 +51,62 @@ def to_json(text: str) -> dict:
         data = orjson.loads(text)
         if isinstance(data, dict):
             return data
-    except orjson.JSONDecodeError:
-        pass
+    except orjson.JSONDecodeError as err:
+        print(err)
     return {}
+
+
+@dataclass
+class SomeForTest:
+    """Some cls for Test."""
+
+    name: str
+    age: int
+    male: bool
+    height: float
+    money: list[float]
+    data: dict
+
+
+class TestUtils:
+    """TestCase for Utils."""
+
+    @staticmethod
+    def test_print2() -> None:
+        """Test print2 beauty print."""
+        data = {"hello": "world", "age": 25, "name": ["Ben", "Cary"], "dict": {}}
+        print2(data=data, extra_line=False)
+        print2(data=data, extra_line=True)
+
+    @staticmethod
+    def test_split() -> None:
+        """Test split list of string or integer."""
+        number = 5
+        list_int = [random.randint(0, 9) for _ in range(10)]
+        group_int = split_list_int(list_int, number=number)
+        assert len(group_int[0]) == number
+        assert len(group_int) * number <= len(list_int)
+
+        list_str = [str(random.randint(0, 9)) for _ in range(10)]
+        group_str = split_list_str(list_str, number=number)
+        assert len(group_str[0]) == number
+        assert len(group_str) * number <= len(list_str)
+
+    @staticmethod
+    def test_to() -> None:
+        """Test to_dict and to_json."""
+        some = SomeForTest(
+            name="name", age=25, male=True, height=10.0, money=[100.0], data={}
+        )
+        dict_some = asdict(some)
+        assert dict_some == to_dict(some)
+
+        str_some = orjson.dumps(dict_some).decode()
+        print(str_some, type(str_some))
+        json_some = to_json(str_some)
+        print(json_some, type(json_some))
+        assert json_some == dict_some
+
+
+if __name__ == "__main__":
+    app = TestUtils()
